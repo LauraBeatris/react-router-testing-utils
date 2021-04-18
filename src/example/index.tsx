d8ba31cc5bfff8218f11a2292617316b629e281b
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-04-18 15:51:06 -0300

    Update app example

--- src/example/index.tsx
+++ src/example/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 
-import { useNavigate } from './useNavigate'
-
-const EXAMPLE_ABOUT_PAGE_ROUTE_NAME = '/about'
-const EXAMPLE_HOME_PAGE_ROUTE_NAME = '/'
+export const EXAMPLE_ABOUT_PAGE_ROUTE_NAME = '/about'
+export const EXAMPLE_HOME_PAGE_ROUTE_NAME = '/'
 
 export const ExampleAboutPage = () => (
   <section data-testid='example-about-page'>
@@ -14,22 +12,18 @@ export const ExampleAboutPage = () => (
   </section>
 )
 
-export const ExampleHomePage = () => {
-  const navigate = useNavigate()
-
-  return (
-    <main data-testid='example-about-page'>
-      <nav>
-        <button type='button' onClick={navigate(EXAMPLE_ABOUT_PAGE_ROUTE_NAME)}>
-          Go to about page
-        </button>
-      </nav>
-      <h1>
-        Example App Title
-      </h1>
-    </main>
-  )
-}
+export const ExampleHomePage = () => (
+  <main data-testid='example-home-page'>
+    <nav>
+      <Link to={EXAMPLE_ABOUT_PAGE_ROUTE_NAME}>
+        Go to about page
+      </Link>
+    </nav>
+    <h1>
+      Example App Title
+    </h1>
+  </main>
+)
 
 export const EXAMPLE_APP_ROUTES = [
   {
@@ -42,12 +36,12 @@ export const EXAMPLE_APP_ROUTES = [
   }
 ]
 
-export const ExampleApp = () => (
-  <BrowserRouter>
+export const ExampleAppRoutes = () => (
+  <>
     {EXAMPLE_APP_ROUTES.map(({ path, component: Component }) => (
       <Route path={path} key={path}>
         <Component />
       </Route>
     ))}
-  </BrowserRouter>
+  </>
 )
