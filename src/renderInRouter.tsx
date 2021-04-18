548250aa763a90dcffe56f2fd64ad77c917b2cbf
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-04-18 11:57:42 -0300

    Add JSDocs to utility

--- src/renderInRouter.tsx
+++ src/renderInRouter.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
+/**
+ * Render a given component in a Router for un-browser environments
+ */
 export const renderInRouter = (Comp: React.FC, initialEntries: ['/']) =>
   render(
     <MemoryRouter initialEntries={initialEntries}>
