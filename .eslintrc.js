374c85cde1fa565a95acb108f4c36a8c08d322f9
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-04-18 12:40:09 -0300

    Configure ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'standard-react',
+    'standard-jsx',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
+  plugins: [
+    'import'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'import/order': ['error', {
+      groups: ['builtin', 'external', 'internal'],
+      'newlines-between': 'always-and-inside-groups'
+    }]
+  },
+  globals: {
+    React: 'writable'
+  }
+}
