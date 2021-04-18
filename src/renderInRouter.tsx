0169a1fbf479e2d773bebd7cf021bf7db93d9c1a
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-04-18 12:01:28 -0300

    Improve renderInRouter to receive named arguments

--- src/renderInRouter.tsx
+++ src/renderInRouter.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import { render } from "@testing-library/react";
+import { render, RenderOptions } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
 /**
  * Render a given component in a Router for un-browser environments
  */
-export const renderInRouter = (Comp: React.FC, initialEntries: ['/']) =>
+export const renderInRouter = ({
+  component: Component, 
+  initialEntries = ['/'], 
+  renderOptions
+}: {
+  component: React.FC,
+  initialEntries: Array<string>,
+  renderOptions: RenderOptions
+}) =>
   render(
     <MemoryRouter initialEntries={initialEntries}>
-      <Comp />
+      <Component />
     </MemoryRouter>
-  );
+  , renderOptions);
