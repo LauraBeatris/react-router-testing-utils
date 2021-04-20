0d8d47b0e6ea1394330bbce14f07fb18d885afd0
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2021-04-20 19:32:10 -0300

    Improve jest config to be in sync with type definition

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import type { Config } from '@jest/types'
+
+const jestConfig: Config.InitialOptions = {
   preset: 'ts-jest',
   setupFilesAfterEnv: [
     '<rootDir>/jest.setup.ts'
   ]
 }
+
+export default jestConfig
